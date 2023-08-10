e2d8fd7416a180e01a69a7752c1db8459a479d45
oyekanwahab <oyekanwahab@gmail.com>
2023-08-10 10:19:47 -0700

    commit for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * This is the main function of the program.
  * It demonstrates printing a message to the console.
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
