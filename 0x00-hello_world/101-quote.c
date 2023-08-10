e03c3e006cf26391a7ebed97d43a228f28771754
oyekanwahab <oyekanwahab@gmail.com>
2023-08-10 15:52:47 -0700

    commit for task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print strings to print out strings
+ *
+ * Return : Always 1 (Success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\"\, \- Dora Korpar\, 2015-10-19\,");
+	return (1);
+}
