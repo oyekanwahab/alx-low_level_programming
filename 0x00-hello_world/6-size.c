99a1972dac15933eb7e037a6a9f290cd60cb9c99
oyekanwahab <oyekanwahab@gmail.com>
2023-08-10 15:11:26 -0700

    commit for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- *Main : this print out size of all types
+ * main : this print out size of all types
  *
  * Return: Alwasys 0
  */
 
 int main(void)
 {
-	printf("Size of a char:%d  byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int:%d bytes(s)\n", sizeof(long));
-	printf("Size of long long :%d bytes(s)\n", sizeof(long long));
-	printf("Size of long float:%d bytes(s)\n", sizeof(float));
+	printf("Size of a char: %d  byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long));
+	printf("Size of long long int: %d bytes(s)\n", sizeof(long long));
+	printf("Size of float: %d bytes(s)\n", sizeof(float));
 }
