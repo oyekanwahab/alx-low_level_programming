c6ecec4ecc8a5f1a5427e6e7f57a66151557a4e1
oyekanwahab <oyekanwahab@gmail.com>
2023-08-11 21:59:39 -0700

    commit for task 5

---
+++ 0x01-variables_if_else_while/6.print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print using for loop.
+ * Description:	'This retirate all the alphabet'
+ *
+ * Return: 0 if sucess
+ *
+ */
+
+int main(void)
+{
+	int num[] = {0,1,2,3,4,5,6,7,8,9}
+
+	for (int i = 0; i <= 9; i++)
+		printf("%d\n", num[i]);
+	return (0);
+}
