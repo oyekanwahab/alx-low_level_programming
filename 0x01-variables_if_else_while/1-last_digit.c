7b526b1138aa707c4c9b2c60c6d5e910406fd4b4
oyekanwahab <oyekanwahab@gmail.com>
2023-08-11 19:37:53 -0700

    commit for task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  prind with modulu
+ *
+ * Return: 0 (Sucess)
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	n % 10 = m
+	if (m > 5)
+		print("Last digit of %d is %d and is greater that 5\n", n, m);
+	else if (m == 0)
+		print("Last digit of %d is %d and is 0\n", n, m);
+	else if (m > 6 || m != 0)
+		print("Last digit of %d is %d and not 0\n", n, m);
+	return (0);
+}
