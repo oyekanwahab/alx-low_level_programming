df8f6753a0d063f5146da301c13da8239eef16b5
oyekanwahab <oyekanwahab@gmail.com>
2023-08-11 20:34:01 -0700

    commit for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+ * main - print character
+ *
+ * Result: 0 if sucessful
+ *
+ */
+
+int main(void)
+{
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y');
+	putchar('z');	
+	return(0);
+}
