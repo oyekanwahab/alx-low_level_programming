42979205638174919f39e42f909c7c1c46fbab93
oyekanwahab <oyekanwahab@gmail.com>
2023-08-11 21:46:31 -0700

    commit for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print using for loop.
+ * Description:	'This retirate all the alphabet'
+ *
+ * Return: 0 if sucess
+ *
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = 0; ch <= 9; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
