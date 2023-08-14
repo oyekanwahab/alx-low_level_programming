2af6677d17332567dfd77ee8b8c64af34ee31176
oyekanwahab <oyekanwahab@gmail.com>
2023-08-14 15:18:16 -0700

    commit for task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/** 
+ * main - print letter with space and comma
+ *
+ * Return: 0 if sucessful
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <='9'; num++) {
+		putchar(num);
+
+		if (num != 9) {
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
