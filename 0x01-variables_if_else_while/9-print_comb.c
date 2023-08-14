2b845f6f6bcc3beeb186918a2be04e5f7be7c3e2
oyekanwahab <oyekanwahab@gmail.com>
2023-08-14 15:21:30 -0700

    commit for task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++) 
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 
-		if (num != 9) 
+		if (num != 9)
 		{
 			putchar(',');
 			putchar(' ');
