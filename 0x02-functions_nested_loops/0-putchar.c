8733f90ee4fa795edb974d080aaf35f519d75904
oyekanwahab <oyekanwahab@gmail.com>
2023-08-15 15:28:33 -0700

    commit for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - print putchar
+ *
+ * Return: 0 if sucessful 
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
