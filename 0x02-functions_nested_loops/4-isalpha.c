96fc7a155fc8eb38f27bfb4937d56b9b75e4e89c
oyekanwahab <oyekanwahab@gmail.com>
2023-08-15 21:29:38 -0700

    commit for task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main"
 
 /**
- * isalpha - check for lower character
+ * _isalpha - check for lower character
  * Description: this check for alphabet
  * @c: the alphabet to check
  *
  * Return: 0 if sucessful
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	return ((if c >= 'a' && c <= 'a') || (c >= 'A' && c <= 'Z')) ;
+	return ((if c >= 'a' && c <= 'a') || (c >= 'A' && c <= 'Z'));
 }
