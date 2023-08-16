9d78a30816d8c7d211e53ef960fb4c901d1ab55e
oyekanwahab <oyekanwahab@gmail.com>
2023-08-15 21:10:02 -0700

    commit for task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * int_islower - check for lower character
+ * Description: this check for lower character
+ * @c: the alphabet to check
+ *
+ * Return: 0 if sucessful
+ */
+
+int _islower(int c)
+{
+	return (if c >= 'a' && c <= 'a');
+}
