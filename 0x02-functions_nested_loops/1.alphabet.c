ecbfe310c54d143eb9b599751839ca9781b4509d
oyekanwahab <oyekanwahab@gmail.com>
2023-08-15 20:17:26 -0700

    commit for task 1

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0 if sucessful
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'c')
+	{
+		_putchar(c);
+		c++;
+	}
+	return (0)
