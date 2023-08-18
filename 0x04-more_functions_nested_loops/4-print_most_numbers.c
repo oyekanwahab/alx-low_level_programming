090a03469789bfec0f5a2f461d941f587de193f1
oyekanwahab <oyekanwahab@gmail.com>
2023-08-17 21:41:36 -0700

    push for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * print_numbers - print number
+ * print_most_numbers - print number
  * Description: 'to print single digit'
  * Return: 0 if succesful or 1 if wrong
  *
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
 	}
 	_putchar('\n');
 }
