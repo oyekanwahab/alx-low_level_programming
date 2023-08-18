177bad4f6e9a509ce9073009ba42d7abc642f879
oyekanwahab <oyekanwahab@gmail.com>
2023-08-17 21:54:51 -0700

    push for task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14
+ * Descriptin: 'printing of 0 to 14 ten times
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+
+	int a, b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar('1');
+				_putchar(b % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
