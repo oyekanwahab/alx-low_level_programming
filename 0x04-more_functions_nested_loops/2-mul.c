a321c2191242321cdfa587a9f02749b11a0a612e
oyekanwahab <oyekanwahab@gmail.com>
2023-08-17 17:19:10 -0700

    commit for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplicatin
+ * Description: multipl two number together 
+ * @a: first number
+ * @b: second number
+ * Return: 0 
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return sum;
+}
