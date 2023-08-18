ee6c8fc1de14186cae820eaba15209ac1d6deaf8
oyekanwahab <oyekanwahab@gmail.com>
2023-08-17 21:29:00 -0700

    push for task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,14 +7,14 @@
  *
  */
 
-int print_numbers(void)
+void print_numbers(void)
 {
 	char c;
 
-	for (c >= 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
+	return;
 }
