e1acd294f08448326cd5d9325b06f1ff83532fae
oyekanwahab <oyekanwahab@gmail.com>
2023-08-17 21:16:08 -0700

    push for task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number
+ * Description: 'to print single digit'
+ * Return: 0 if succesful or 1 if wrong
+ *
+ */
+
+int print_numbers(void)
+{
+	char c;
+
+	for (c >= 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
