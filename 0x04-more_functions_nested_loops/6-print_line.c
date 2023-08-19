50278fbce04445dc4908a9f98d411168e3eb4635
oyekanwahab <oyekanwahab@gmail.com>
2023-08-18 18:10:48 -0700

    commit for task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * Description: 'print # multiple times'
+ * @n: integer to print #
+ * return: no return 
+ */
+
+void print_line(int n)
+{
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+}
