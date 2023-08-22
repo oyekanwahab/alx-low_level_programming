4ac1e97c01f78a965324a4537d378334859672ed
oyekanwahab <oyekanwahab@gmail.com>
2023-08-21 19:29:19 -0700

    commit for task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update vale 
+ * Description: 'this take a pointer to an int as parameter and updates the value it points to to 98' 
+ * @*n: point to chage to
+ * Return: Always 0
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
