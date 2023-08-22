934d62184ec0b51a997fb105be828de2ffe64cc8
oyekanwahab <oyekanwahab@gmail.com>
2023-08-21 20:43:07 -0700

    commit for task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - len of a str
+ * Description: 'print out lenght of string'
+ * @s: String to print out lenght 
+ * * Return: Always 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return l;
+
+
+}
