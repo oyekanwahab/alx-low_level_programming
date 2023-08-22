74d1493c6671ff7bb502a4923244c9101667ac40
oyekanwahab <oyekanwahab@gmail.com>
2023-08-22 11:55:49 -0700

    commit for task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - print out word in reverse
+ * Description: 'print out of string'
+ * @s: String to print out lenght
+ * Return: Always 0
+ *
+ */
+
+void rev_string(char *s)
+{
+	int c = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+
+	
+	for (i = c - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
