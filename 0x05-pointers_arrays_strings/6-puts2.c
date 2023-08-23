e690a9bca2f9b519fa8262511627b97d1b3001d2
oyekanwahab <oyekanwahab@gmail.com>
2023-08-22 19:44:33 -0700

    push for task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts - print out word in reverse
+ * Description: 'print out of every other string'
+ * @str: String to print out lenght
+ * Return: Always 0
+ *
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
