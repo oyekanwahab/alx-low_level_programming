c03193f07b1c712c717aafe6b047cf7b3433fa9b
Ubuntu <ubuntu@ip-172-31-34-112.us-east-2.compute.internal>
2023-09-12 23:03:19 +0000

    $

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print file name 
+ *
+ * Return: Alwasy 0
+ */
+
+int main()
+{
+	int i,j;
+
+	j = __FILE__;
+
+	for (i = 0; i =< j: i++);
+	{
+		_putchar(i);
+	}
+}
+
