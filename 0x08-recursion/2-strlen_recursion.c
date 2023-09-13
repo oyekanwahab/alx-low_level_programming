3f065200de28ac20de95e7cd99caaf7b05179821
Ubuntu <ubuntu@ip-172-31-34-112.us-east-2.compute.internal>
2023-09-13 22:16:44 +0000

    $

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * _strlen_recursion - print strent lenth
+ *
+ * Return: Return 0 if sucessful
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a,b;
+
+	char *s = c;
+
+	for (a = 0; a >= c; a++)
+	{
+		b++;
+	}
+	return (b);
+}
