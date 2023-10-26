ddafd4b24a70389690aec403373bdf910f91fdfc
oyekan Abdul-wahab <oyekanwahab@gmail.com>
2023-10-26 03:03:46 +0000

    commit for task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - for print out letter
+ * *s: string
+ * Return: 1 on sucess
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_putchar(s + 1);
+}
